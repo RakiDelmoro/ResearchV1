9181e8c617688737f38f886d4bf8e115bfeeabc9
Raki Delmoro <rakidelmoro@gmail.com>
2024-09-22 13:38:25 +0000

    Fixed typo

--- alternative_backprop.py
+++ alternative_backprop.py
@@ -29,13 +29,13 @@ def neural_network(input_data, expected_data):
         last_layer_forward_pass = (cp.dot(hidden_2_neurons_forward_pass, hidden_2_to_output_weights)) + output_bias
         # backward pass
         hidden_2_neurons_backward_pass = (cp.dot(last_layer_forward_pass, expected_to_hidden_2_weights)) + hidden_2_bias_backward
-        hidden_2_neurons_backward_pass = (cp.dot(hidden_2_neurons_backward_pass, hidden_2_to_hidden_1_weights)) + hidden_1_bias_backward
+        hidden_1_neurons_backward_pass = (cp.dot(hidden_2_neurons_backward_pass, hidden_2_to_hidden_1_weights)) + hidden_1_bias_backward
         last_layer_backward_pass = (cp.dot(hidden_2_neurons_backward_pass, hidden_1_to_input_data_weights)) + input_bias_backward
 
         # Errors for each layer
         error_for_output_neurons = last_layer_forward_pass - expected_data
         error_for_hidden_2_neurons = hidden_2_neurons_backward_pass - hidden_2_neurons_forward_pass
-        error_for_hidden_1_neurons = hidden_2_neurons_backward_pass - hidden_1_neurons_forward_pass
+        error_for_hidden_1_neurons = hidden_1_neurons_backward_pass - hidden_1_neurons_forward_pass
         error_for_input_neurons = last_layer_backward_pass - input_data
 
         # Network stress is sum of forward pass layers and backward pass layers difference squared
@@ -55,7 +55,6 @@ def neural_network(input_data, expected_data):
         print(f"input neuron: {last_layer_backward_pass} input: {input_data}")
         print(f"output neuron {last_layer_forward_pass} expected: {expected_data}")
         print(f"Network stress: {cp.mean(network_stress)}")
-
         epochs += 1
 
 x = cp.random.randn(1, 2)
