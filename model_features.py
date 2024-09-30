bae4adb05040effaaaf20d0f55270a90e7104b7f
Raki Delmoro <rakidelmoro@gmail.com>
2024-09-30 03:41:18 +0000

    Update

--- model_features.py
+++ model_features.py
@@ -1,3 +1,10 @@
+import torch
+
+DEVICE = "cuda"
 EPOCHS = 10
 BATCH_SIZE = 2098
 LEARNING_RATE = 6.0
+MODEL_FEATURE_SIZE = [784, 2000, 10]
+LOSS_FUNCTION = torch.nn.CrossEntropyLoss()
+HIDDEN_LAYER_ACTIVATION = torch.nn.LeakyReLU()
+OUTPUT_LAYER_ACTIVATION = torch.nn.Softmax(dim=-1)
